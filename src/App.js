073fd0b26a5ff3e7784a5507eaedfbe5804f3acf
asikhsingh <amritpalasingh@outlook.com>
2022-08-23 00:59:21 -0400

    header-cartButton-img

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Layout/Header.js";
+
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
