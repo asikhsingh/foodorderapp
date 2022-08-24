3dce88fa6fb3968ca71457f4bca15ab9c11f333e
asikhsingh <amritpalasingh@outlook.com>
2022-08-24 00:54:27 -0400

    add forms

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import Header from "./components/Layout/Header.js";
 
 import React, { Fragment } from "react";
 
+import Meals from "./components/Meals/Meals.js";
+
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
