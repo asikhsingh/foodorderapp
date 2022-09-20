a5a46b261f3748930d7aaa90c5e2bfcc1163be4b
asikhsingh <amritpalasingh@outlook.com>
2022-09-20 16:05:28 -0400

    last update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC7qoEA-K5aE7R7rilShIKyWGi6tk1SKW4",
+//   authDomain: "clone-9596b.firebaseapp.com",
+//   projectId: "clone-9596b",
+//   storageBucket: "clone-9596b.appspot.com",
+//   messagingSenderId: "462050672066",
+//   appId: "1:462050672066:web:1c97f93edbf26c3f07ea5b",
+// };
